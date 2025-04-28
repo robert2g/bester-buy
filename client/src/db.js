15ee185be5f8c79176d66252ba9676f5d3941a0d
Robert G. <jose.gglassman@proton.me>
2025-04-28 16:35:01 -0500

    Update db.js
    Removing old secrets

--- client/src/db.js
+++ client/src/db.js
@@ -3,13 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA6xESxkXcQEf6_M_rj0xyn8DAz43GtJHA",
-    authDomain: "besterbuy-c18f6.firebaseapp.com",
-    projectId: "besterbuy-c18f6",
-    storageBucket: "besterbuy-c18f6.appspot.com",
-    messagingSenderId: "99528590354",
-    appId: "1:99528590354:web:c8adbc9de1acc507e6c4cb",
-    measurementId: "G-LV5T03YQZD"
+// Removed as project no longer requires access to non-existent firebase instance.
   };
 
 
